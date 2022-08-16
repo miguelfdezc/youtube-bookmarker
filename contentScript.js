710322a980d34409e287d82e2fd40c150bbe8f2b
miguelfdezc <miguefdezc99@gmail.com>
2022-08-16 16:22:12 +0300

    Added condition to prevent it from updating the URL in non-YouTube websites

--- contentScript.js
+++ contentScript.js
@@ -62,7 +62,7 @@
   });
 
   let trail="&ytExt=ON";
-  if(!window.location.href.includes(trail) && !window.location.href.includes("ab_channel")){
+  if(!window.location.href.includes(trail) && !window.location.href.includes("ab_channel") && window.location.href.includes("youtube.com/watch")){
     window.location.href += trail;
   }
 })();
